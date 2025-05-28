5e78534700694c5a113ad790e49640429dbcff91
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-05-28 15:26:30 +0200

    [UPD] App.js aggiunta posts

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const postsRouter = require("./router/posts.js");
+
+app.use(express.static("public"));
+app.use(express.json());
+
+//Entry Point
+app.get("/", (req, res) => {
+    console.log("Server del mio Blog")
+    res.send("Benvenuti nel mio Blog")
+});
+
+app.use("/posts", postsRouter);
+
+app.listen(port, () => {
+    console.log(`server in ascolto alla porta ${port}`);
+});
