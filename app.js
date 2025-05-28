574ca263ae7954bcaf1fe1b508aadf2e1d13a8cc
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-05-28 16:07:07 +0200

    [UPD] Fixato l'App.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+//Importo router
 const postsRouter = require("./router/posts.js");
 
 app.use(express.static("public"));
