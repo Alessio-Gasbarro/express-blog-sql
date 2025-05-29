cf12690dbdd416f842768ee56e20952a1117e9c1
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-05-29 10:11:30 +0200

    [UPD] Added Bonus in postsController - show

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -12,16 +12,36 @@ const index = (req, res) => {
 
 // SHOW
 const show = (req, res) => {
-    const id = req.params.id;
-    connection.query("SELECT * FROM posts WHERE id = ?", [id], (err, results) => {
-        if (err) {
-            return res.status(500).json({ error: "Errore nella query: " + err });
-        }
-        if (results.length === 0) {
-            return res.status(404).json({ message: "Post non trovato" });
-        }
-        res.json(results[0]);
-    });
+    const { id } = req.params;
+
+    const postSql = "SELECT * FROM posts WHERE id = ?";
+    const tagSql = `SELECT * FROM tags JOIN post_tag ON post_tag.tag_id = tag.id WHERE post_tag.post_id = ?`
+
+    connection.query(postSql, [id], (err, result) => {
+        if (err) return res.status(500).json({ error: "Database query failed: " + err });
+
+        if (result.length === 0) return res.status(404).json({ error: "Post non trovato" })
+
+        const post = result[0];
+
+        connection.query(tagSql, [id], (err, tagResult) => {
+            if (err) return res.status(500).json({ error: "Database query failed: " + err });
+
+            post.tags = tagResult;
+
+            res.json(post);
+        })
+    })
+    //const id = req.params.id;
+    //connection.query("SELECT * FROM posts WHERE id = ?", [id], (err, results) => {
+    //    if (err) {
+    //        return res.status(500).json({ error: "Errore nella query: " + err });
+    //    }
+    //    if (results.length === 0) {
+    //        return res.status(404).json({ message: "Post non trovato" });
+    //    }
+    //    res.json(results[0]);
+    //});
 };
 
 // DESTROY
